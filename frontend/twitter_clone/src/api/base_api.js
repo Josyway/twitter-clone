8542bda4ed1bec4bf681e1b6997a5afd0085424d
Josyway Paulo <107008079+Josyway@users.noreply.github.com>
2025-04-12 16:20:22 -0300

    Update base_api.js

--- frontend/twitter_clone/src/api/base_api.js
+++ frontend/twitter_clone/src/api/base_api.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = ['http://localhost:8000', 'https://josyway.pythonanywhere.com/' ];
+export const API_BASE_URL = ['https://Josyway.pythonanywhere.com/', 'https://josyway.pythonanywhere.com/' ];
